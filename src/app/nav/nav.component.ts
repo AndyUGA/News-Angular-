e457d03658ae329cb3c1ae43ff44b19454c74739
Andy Truong <att35663@uga.edu>
2019-08-03 13:24:38 -0400

    Moved files ..

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  public test: any = [];
+  constructor(private http:HttpClient) {
+
+
+   }
+
+  ngOnInit() {
+    let apiURL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0c69f9ba9a494e84a33b2749dab3fc5e";
+   
+
+    let testData = this.http.get(apiURL).subscribe(result => this.test = result);
+    
+     
+  }
+
+}
